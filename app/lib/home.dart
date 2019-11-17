8e87cfe94b0d678134ff01f91703445636388df8
Stanley Heryanto <stanleyheryanto@Gmail.com>
2019-11-17 17:01:22 +0700

    Home Scaffolding

--- app/lib/home.dart
+++ app/lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sehatia/home_page.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,11 +7,75 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _index = 0;
+
+  List<Widget> _pages = [
+    HomePage(),
+    SizedBox.shrink(),
+    SizedBox.shrink(),
+    SizedBox.shrink(),
+  ];
+
+  List<String> _titles = [
+    'Home',
+    'Periksa',
+    'Riwayat',
+    'Profil',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      
+      appBar: AppBar(
+        title: Text(
+          _titles[_index],
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.notifications),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: _pages[_index],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _index,
+        onTap: (index) {
+          setState(() {
+            _index = index;
+          });
+        },
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        items: [
+          BottomNavigationBarItem(
+            title: Text('Home'),
+            icon: Image.asset('img/Home.png'),
+            activeIcon: Image.asset('img/HomeActive.png'),
+          ),
+          BottomNavigationBarItem(
+            title: Text('Periksa'),
+            icon: Image.asset('img/Periksa.png'),
+            activeIcon: Image.asset('img/PeriksaActive.png'),
+          ),
+          BottomNavigationBarItem(
+            title: Text('Riwayat'),
+            icon: Image.asset('img/Riwayat.png'),
+            activeIcon: Image.asset('img/RiwayatActive.png'),
+          ),
+          BottomNavigationBarItem(
+            title: Text('Profil'),
+            icon: Image.asset('img/Profil.png'),
+            activeIcon: Image.asset('img/ProfilActive.png'),
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ app/lib/home_page.dart
@@ -0,0 +1,216 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _nama = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchData();
+  }
+
+  void _fetchData() async {
+    FirebaseUser user = await FirebaseAuth.instance.currentUser();
+    setState(() {
+      _nama = user.displayName;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Selamat pagi $_nama,\nJangan lupa minum obat batukmu ya',
+                  style: TextStyle(fontSize: 18, color: Colors.grey),
+                ),
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              GestureDetector(
+                onTap: () {},
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 16, horizontal: 22),
+                    child: Column(
+                      children: <Widget>[
+                        Image.asset('img/ic_periksa.png'),
+                        SizedBox(height: 8),
+                        Text(
+                          'Periksa',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 16, horizontal: 22),
+                    child: Column(
+                      children: <Widget>[
+                        Image.asset('img/ic_imunisasi.png'),
+                        SizedBox(height: 8),
+                        Text(
+                          'Imunisasi',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 16, horizontal: 22),
+                    child: Column(
+                      children: <Widget>[
+                        Image.asset('img/ic_kb.png'),
+                        SizedBox(height: 8),
+                        Text(
+                          'KB',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              GestureDetector(
+                onTap: () {},
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 16, horizontal: 10),
+                    child: Column(
+                      children: <Widget>[
+                        Image.asset('img/ic_obat.png'),
+                        SizedBox(height: 8),
+                        Text(
+                          'Konsumsi Obat',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 16, horizontal: 16),
+                    child: Column(
+                      children: <Widget>[
+                        Image.asset('img/ic_rekam.png'),
+                        SizedBox(height: 8),
+                        Text(
+                          'Rekam Medis',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 16, horizontal: 22),
+                    child: Column(
+                      children: <Widget>[
+                        Image.asset('img/ic_more.png'),
+                        SizedBox(height: 8),
+                        Text(
+                          'Lainnya',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 24),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: <Widget>[
+                SizedBox(width: 16),
+                Container(
+                  height: 160,
+                  width: 300,
+                  color: Colors.orange,
+                ),
+                SizedBox(width: 16),
+                Container(
+                  height: 160,
+                  width: 300,
+                  color: Colors.green,
+                ),
+                SizedBox(width: 16),
+                Container(
+                  height: 160,
+                  width: 300,
+                  color: Colors.yellow,
+                ),
+                SizedBox(width: 16),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
